5012a89dcd7f240b01b0ae67245234534245f360
natta yuki <yuki1118nitta@gmail.com>
2019-04-30 13:12:09 +0000

    Articlrteテーブルにuser_idカラムを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :move_to_index, except: :index
+  #move_to_indexをnewとcreateの前のみ動かす
   def index
     @articles = Article.all.order("id DESC").page(params[:page]).per(7)
   end
@@ -7,11 +9,17 @@ class BlogsController < ApplicationController
   end
   
   def create
-    Article.create(blog_params)
+    Article.create(title: blog_params[:title], image: blog_params[:image], text: blog_params[:text], user_id: current_user.id)
+    #(blog_params)
   end
 
   private
   def blog_params
     params.permit(:title, :image, :text)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+    #ユーザーがログインしてないとき、indexアクションを実行
   end
 end
