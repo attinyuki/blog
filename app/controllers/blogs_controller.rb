6638665c116e2b11b5d8048fcffc3a07059fba13
natta yuki <yuki1118nitta@gmail.com>
2019-04-30 06:16:34 +0000

    blogs/newで投稿画面へ

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,9 @@
 class BlogsController < ApplicationController
   def index
   end
+  
+  def new
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'blogs' => 'blogs#index'
+  get 'blogs' => 'blogs#index'  #一覧画面
+  get 'blogs/new' => 'blogs#new' #投稿画面
 
 
 end
