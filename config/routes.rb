52b97353e7c286d3168375d49d7838eb594e5c3b
natta yuki <yuki1118nitta@gmail.com>
2019-04-30 08:06:43 +0000

    ルートパスで投稿一覧へ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'blogs#index' #ルートパス指定
   get 'blogs' => 'blogs#index'  #一覧画面
   get 'blogs/new' => 'blogs#new' #投稿画面
   post 'blogs' => 'blogs#create' #投稿機能
