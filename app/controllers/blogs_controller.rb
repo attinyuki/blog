8d1f161154a1451724f6badadd18817e76211ea7
natta yuki <yuki1118nitta@gmail.com>
2019-05-15 04:55:02 +0000

    削除機能のdestroyアクションを追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,6 +11,14 @@ class BlogsController < ApplicationController
   def create
     Article.create(title: blog_params[:title], image: blog_params[:image], text: blog_params[:text], user_id: current_user.id)
   end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.user_id == current_user.id
+      @article.destroy
+    end
+  end
+  
 
   private
   def blog_params
